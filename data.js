f40c68714a2e623a046d7bb82d12f51d24571ab5
Alex Buzevych <alex@mail.ru>
2017-05-18 20:25:41 +0300

    second1234567

--- data.js
+++ data.js
@@ -1,8 +1,52 @@
 $(document).ready(function() {
 	
-	var x = document.cookie;
+	var token = $.cookie('token');
 	
-	alert( $.cookie('token'));
+	getData(token);
 	
 
 });
+
+
+function signOut(){
+	
+	$.removeCookie('token');
+	
+}
+
+
+function getData(var token){
+	
+		var XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
+
+		var xhr = new XHR();
+
+		// (2) запрос на другой домен :)
+		xhr.open('GET', 'https://jerseybuzzapp.herokuapp.com/webapi/shoes/get/'+token, true);
+
+		xhr.onload = function() {
+			var obj = this.responseText;
+			//alert( obj);
+			var event = JSON.parse(obj);
+
+			if(event["status"] =="OK"){
+				
+				$("#humidity").html(event["humidity"]);
+				$("#temperature").html(event["temperature"]);
+			}else{
+				alert(event["errorValue"]);
+				
+			}
+		  //window.location.href="data.html";
+		}
+
+		xhr.onerror = function() {
+		  alert( 'Ошибка ' + this.status );
+		}
+
+		xhr.send();
+	
+	
+	
+	
+}
