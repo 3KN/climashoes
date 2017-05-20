3482987a9cf06d7d2dc738e3db61d618faf63512
Alex Buzevych <alex@mail.ru>
2017-05-20 21:19:39 +0300

    subota3

--- steps.js
+++ steps.js
@@ -55,24 +55,6 @@ function getSteps(token){
 }
 
 
-function makeUL(array) {
-    // Create the list element:
-    var list = document.createElement('ul');
-
-    for(var i = 0; i < array.length; i++) {
-        // Create the list item:
-        var item = document.createElement('li');
-
-        // Set its contents:
-        item.appendChild(document.createTextNode(array[i]["date"] + " "+ array[i]["steps"]);
-
-        // Add it to the list:
-        list.appendChild(item);
-    }
-
-    // Finally, return the constructed list:
-    return list;
-}
 
 
 function makeLI(jsonArr) {
