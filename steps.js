ee8f725c6b38d4f5d7e5619b0ff84c99198ced7b
Alex Buzevych <alex@mail.ru>
2017-05-20 21:14:20 +0300

    subota2

--- steps.js
+++ steps.js
@@ -16,6 +16,8 @@ function signOut(){
 
 function getSteps(token){
 	
+	
+	
 	var XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
 
 		var xhr = new XHR();
@@ -30,8 +32,10 @@ function getSteps(token){
 
 			if(event["status"] =="OK"){
 				
-				//var dates = JSON.parse(event["dates"]);
-				alert(event);
+				var jsonArr = event["dates"];
+				makeLI(jsonArr);
+				
+				
 				
 			}else{
 				alert(event["errorValue"]);
@@ -46,8 +50,40 @@ function getSteps(token){
 
 		xhr.send();
 	
+
 	
+}
+
+
+function makeUL(array) {
+    // Create the list element:
+    var list = document.createElement('ul');
+
+    for(var i = 0; i < array.length; i++) {
+        // Create the list item:
+        var item = document.createElement('li');
+
+        // Set its contents:
+        item.appendChild(document.createTextNode(array[i]["date"] + " "+ array[i]["steps"]);
+
+        // Add it to the list:
+        list.appendChild(item);
+    }
+
+    // Finally, return the constructed list:
+    return list;
+}
+
+
+function makeLI(jsonArr) {
 	
-	
-	
+	var element = document.getElementById("kekcheb");
+ 	for (i = 0; i < jsonArr.length; i++) { 
+		
+		var li=document.createElement('li');
+		li.innerHTML=li.innerHTML + jsonArr[i]["date"] + " "+ jsonArr[i]["steps"];
+		element.appendChild(li);
+	 
+	}
+
 }
