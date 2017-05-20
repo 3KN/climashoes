758d183be3c91504553b70cc03aec3406af85f4e
Alex Buzevych <alex@mail.ru>
2017-05-20 20:02:55 +0300

    subota2

---
+++ steps.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+	
+	var token = $.cookie('token');
+	
+	getSteps(token);
+	
+
+});
+
+function signOut(){
+	
+	$.removeCookie('token');
+	
+}
+
+
+function getSteps(token){
+	
+	var XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
+
+		var xhr = new XHR();
+
+		// (2) запрос на другой домен :)
+		xhr.open('GET', 'https://jerseybuzzapp.herokuapp.com/webapi/steps/get/'+token, true);
+
+		xhr.onload = function() {
+			var obj = this.responseText;
+			//alert( obj);
+			var event = JSON.parse(obj);
+
+			if(event["status"] =="OK"){
+				
+				alert(event["dates"]);
+				
+			}else{
+				alert(event["errorValue"]);
+				
+			}
+		  //window.location.href="data.html";
+		}
+
+		xhr.onerror = function() {
+		  alert( 'Ошибка ' + this.status );
+		}
+
+		xhr.send();
+	
+	
+	
+	
+	
+}
